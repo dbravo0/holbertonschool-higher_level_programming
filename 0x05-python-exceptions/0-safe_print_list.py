096627ae1c2c8b3021e97e570df868375275e502
David <stevenbravobel@gmail.com>
2020-01-09 23:42:45 +0000

    [Feature] file 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            c += 1
+
+        except IndexError:
+            print()
+            return c
+
+    print()
+    return c
