20c226571f4abd9e7e0fb874752dc23109e72bf2
David <stevenbravobel@gmail.com>
2020-01-10 03:40:57 +0000

    [Fix] file 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_integer_err(value):
-    import sys
-    try:
-        print("{:d}".format(value))
-        return True
-    except Exception as errex:
-        print("Exception: {}".format(errex), file=sys.stderr)
-        return False
+class Node:
+    def __init__(self, data, next_node=None):
+        def data(self):
+            def data(self, value):
+                if type(data) != int:
+                    raise TypeError
+                def next_node(self):
+                    def next_node(self, value):
