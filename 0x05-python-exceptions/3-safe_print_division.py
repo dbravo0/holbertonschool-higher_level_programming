91385ab9c37380b4086c9556ceae995090b620bf
David <stevenbravobel@gmail.com>
2020-01-10 05:43:45 +0000

    [Feature] file 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    c = None
+    try:
+        c = a / b
+
+    except:
+        pass
+
+    finally:
+        print("Inside result: {}".format(c))
+        return c
