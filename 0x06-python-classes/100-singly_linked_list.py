00975a76194eb791cb48a8e7312c752343ddfca0
David <stevenbravobel@gmail.com>
2020-01-10 03:58:32 +0000

    [Fix] file 6

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 class Node:
     def __init__(self, data, next_node=None):
-        def data(self):
-            def data(self, value):
-                if type(data) != int:
-                    raise TypeError
-                def next_node(self):
-                    def next_node(self, value):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
-    @property
-    def size(self):
-        return self.__size
+    def area(self):
+        return (self.__size ** 2)
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            print("\n" * self.__position[1], end="")
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print('#' * self.__size)
 
     @property
     def position(self):
@@ -22,17 +22,30 @@ class Square:
 
     @position.setter
     def position(self, value):
-
+        if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-
-    def area(self):
-        return self.__size * self.__size
-
-    def my_print(self):
-        if self.size == 0:
-            print()
+        elif (len(value) is not 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(value[0]) is not int or
+              type(value[1]) is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(value[0]) is str or
+              type(value[1]) is str):
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            for c in range(self.size):
-                for c in range(self.size):
-                    print('#', end='')
-                print()
+            self.__position = value
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
