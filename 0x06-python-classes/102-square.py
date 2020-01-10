c1715bed87590f0de8261c5b582844e80ede2e3b
David <stevenbravobel@gmail.com>
2020-01-10 03:27:09 +0000

    [Feature] file 102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def __eq__(self, other):
+        return ((self.size) == (other.size))
+
+    def __ne__(self, other):
+        return ((self.size) != (other.size))
+
+    def __lt__(self, other):
+        return ((self.size) < (other.size))
+
+    def __le__(self, other):
+        return ((self.size) <= (other.size))
+
+    def __gt__(self, other):
+        return ((self.size) > (other.size))
+
+    def __ge__(self, other):
+        return ((self.size) >= (other.size))
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
