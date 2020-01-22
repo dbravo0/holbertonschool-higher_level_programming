5931632730c88a1ef9b84bbae771efd7878dade7
David <stevenbravobel@gmail.com>
2020-01-22 02:51:10 +0000

    [Feature] files 1

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_of_lines(filename=""):
+
+    lines = 0
+
+    with open(filename, encoding="utf-8") as f:
+        for i in f:
+            lines += 1
+
+    return lines
