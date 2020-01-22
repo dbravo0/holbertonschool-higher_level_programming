ce380ccd690f9fea6388981e217ee36c0953adbe
David <stevenbravobel@gmail.com>
2020-01-22 02:57:54 +0000

    [Feature] files 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    with open(filename, encoding="utf-8") as f:
+        if nb_lines <= 0:
+            print(f.read(), end="")
+        for i in range(nb_lines):
+            print(f.readline(), end="")
