ecbcc3573cd67c5558745fbbbdb08b0119265e99
David <stevenbravobel@gmail.com>
2020-05-06 01:01:31 +0000

    [Fix] file 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const counter = parseInt(process.argv[2]);
-if (isNaN(counter)) {
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let idx = 0 ; idx < counter ; idx++) {
+  for (let idx = 0 ; idx < parseInt(process.argv[2]) ; idx++) {
     console.log('C is fun');
   }
 }
