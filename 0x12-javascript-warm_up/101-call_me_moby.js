c17fa72e9a0d6363a89ff99f052a6e87dea59653
David <stevenbravobel@gmail.com>
2020-05-06 00:56:03 +0000

    [Fix] files 8-101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.callMeMoby = function callMeMoby (x, theFunction) {
-  for (let idx = 0 ; idx < x ; idx++) {
+  for (let idx = 0; idx < x; idx++) {
     theFunction();
   }
 };
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const counter = parseInt(process.argv[2]);
 if (isNaN(counter)) {
   console.log('Missing size');
 } else {
-  for (let idx = 0 ; idx < counter ; idx++) {
+  for (let idx = 0; idx < counter; idx++) {
     console.log('X'.repeat(counter));
   }
 }
