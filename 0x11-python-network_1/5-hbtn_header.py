5c37d127d9495e75572b2ad9a752a368f62a4fb1
David <stevenbravobel@gmail.com>
2020-08-25 03:04:35 +0000

    [Fix] file 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a request to the URL and displays the value of the variable X-Request-Id"""
+"""Sends a request to an URL and displays the value of variable X-Request-Id"""
 
 import requests
 import sys
