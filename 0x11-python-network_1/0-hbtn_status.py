70b5fe5b4e622e057f36fd0c38b6a010ebcf4f4b
David <stevenbravobel@gmail.com>
2020-08-25 02:29:09 +0000

    [Fix] file 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
 
-import urllib.request
 
-
-with urllib.request.urlopen("https://intranet.hbtn.io/status") as url:
-    page = url.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(page)))
-    print("\t- content: {}".format(page))
-    print("\t- utf8 content: {}".format(page.decode()))
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as url:
+        page = url.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode()))
