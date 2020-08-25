5e8438212cb0a8f738f0b0c1d0cd668feca6ccc0
David <stevenbravobel@gmail.com>
2020-08-25 04:03:50 +0000

    [Feature] file 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Uses Github API to list the 10 most recent commits"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = requests.get("https://api.github.com/repos/{}/{}/commits"
+                        .format(argv[2], argv[1]))
+    for commit in url.json()[:10]:
+        print('{}: {}'.format(commit.get('sha'), commit.get('commit')
+                              .get('author').get('name')))
