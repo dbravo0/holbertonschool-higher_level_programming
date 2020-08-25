073454b34ed0989aca7d76546508c10d61ac1de7
David <stevenbravobel@gmail.com>
2020-08-25 02:04:51 +0000

    [Feature] file 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Fetches https://intranet.hbtn.io/status
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as url:
+        page = url.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode('utf-8')))
