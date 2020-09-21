37346a71e3abacefd6d734042c0525e607a636b7
David <stevenbravobel@gmail.com>
2020-09-21 17:33:21 +0000

    [Feature] file 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const Square2 = require('./5-square');
+
+module.exports = class Square extends Square2 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0 ; i < this.height ; i++) { console.log(c.repeat(this.width)); }
+  }
+}
