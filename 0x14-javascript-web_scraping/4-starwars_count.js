a85f78c0f3e59bb108f0176ffe2a36234a70fc2c
David <stevenbravobel@gmail.com>
2020-09-22 21:05:30 +0000

    [Feature] file 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+    const films = JSON.parse(body).results;
+    let c = 0;
+    for (const data of films) {
+      for (const str of films[data].characters) {
+        if (films[data].characters[str],includes('18')) {
+          n += 1;
+        }
+      }
+    }
+    console.log(n);
+});
