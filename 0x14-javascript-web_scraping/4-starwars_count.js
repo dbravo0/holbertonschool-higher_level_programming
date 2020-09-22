aa5605ceebfe5a94e7c0b7b7ef869fb4719b77b4
David <stevenbravobel@gmail.com>
2020-09-22 21:09:37 +0000

    [Fix] file 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
-
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
+request(url, (error, response, body) => {
   if (error) {
-    console.log(error);
+    return console.log(error);
   }
-    const films = JSON.parse(body).results;
-    let c = 0;
-    for (const data of films) {
-      for (const str of films[data].characters) {
-        if (films[data].characters[str],includes('18')) {
-          c += 1;
-        }
+  const films = JSON.parse(body).results;
+  let c = 0;
+  for (const data in films) {
+    for (const str in films[data].characters) {
+      if (films[data].characters[str].includes('18')) {
+        c += 1;
       }
     }
-    console.log(c);
+  }
+  console.log(c);
 });
