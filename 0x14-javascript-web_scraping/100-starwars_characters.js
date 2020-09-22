3e6eaf305ea40499459b478a6f3970be1ee1292c
David <stevenbravobel@gmail.com>
2020-09-22 21:21:54 +0000

    [Fix] file 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,13 +3,19 @@
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const request = require('request');
 
-request(url, (error, response, body) => {
-  if (error) { console.log(error); }
-  const characters = JSON.parse(body).characters;
-  for (const data of characters) {
-    request(data, funtion (error, response, body) {
-      if (error) { console.log(error); }
-      console.log(JSON.parse(body).name);
-    });
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const data of characters) {
+      request(data, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
 });
